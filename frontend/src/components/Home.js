73560924ffe16d09e75f965c53f23893256df4ac
Joseph Urschler <jmuwf4@mail.missouri.edu>
2020-12-11 12:36:07 -0800

    installed several necessary dependencies to lay groundwork

---
+++ frontend/src/components/Home.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+/**
+ *  DESCRIPTION: 
+ *  FLOW: App => Home
+ *  PARENT: App
+ *  CHILDREN: none
+ */
+
+function Home() {
+
+  return (
+
+    <div>
+      Home
+    </div>
+
+  );
+
+}
+
+
+export default Home;
